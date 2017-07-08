e82da531c564520ec07eaa5762bdd78afadaeee2
Rigo B Castro <rigobcastro@gmail.com>
2017-07-08 01:41:57 -0500

    Use PropTypes as separated package

--- src/after-interactions.js
+++ src/after-interactions.js
@@ -1,4 +1,5 @@
-import {Component, PropTypes} from 'react';
+import {Component} from 'react';
+import PropTypes from 'prop-types';
 import {InteractionManager} from 'react-native';
 
 export default class AfterInteractions extends Component {
